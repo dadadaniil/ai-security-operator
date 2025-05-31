0dc04a34aea693ce603f77938d9ddb65259987d4
dadadaniil <den382505@gmail.com>
2025-05-31 20:50:20 +0300

    Update hook_router.py
    Prepare for issues hook consumption

--- analyzer/api/routers/hook_router.py
+++ analyzer/api/routers/hook_router.py
@@ -1,21 +1,38 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
-import os
-import shutil
-from typing import Optional
+from fastapi import APIRouter, UploadFile
 
-from api.data.db import *
 from api.data.vectorstore import *
-from api.data.types import DataType
-from api.dto import DocumentInfo
+from api.routers.document_router import upload_and_index_document
 
 logger = logs.get_logger(__name__)
 
 router = APIRouter(prefix="/hook", tags=["Hooks for GitHub and other services"])
 
 
-# Source Code Routes
 @router.post("/github",
           description='Uploads source code for project to generate tests and summary for',
           response_model=dict)
-async def upload_source_code(src):
-    logger.info(f"Yay: {str(src)}")
+async def process_hook(src: dict):
+    action = src['action']
+
+    # New TR issue opened
+    if action == 'opened':
+        issue = src['issue']
+
+        body = issue['body']
+        filename = issue['url']
+
+        with open(filename, 'wb', encoding='utf-8') as f:
+            f.write(body)
+
+            upload_file = UploadFile(f)
+
+            await upload_and_index_document(upload_file, DataType.REQUIREMENTS)
+
+    # todo process push webhook
+    # for commit in src['commits']:
+    #   for file in commit['added']:
+    #       add
+    #   for file in commit['removed']:
+    #       remove
+    #   for file in commit['modified']:
+    #       remove, add
