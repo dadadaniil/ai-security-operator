77fb446857b35bec5e2fca9df9170f8af09b32b1
dadadaniil <den382505@gmail.com>
2025-06-01 13:44:57 +0300

    Update hook_router.py

--- analyzer/api/routers/hook_router.py
+++ analyzer/api/routers/hook_router.py
@@ -8,31 +8,38 @@ logger = logs.get_logger(__name__)
 router = APIRouter(prefix="/hook", tags=["Hooks for GitHub and other services"])
 
 
-@router.post("/github",
-          description='Uploads source code for project to generate tests and summary for',
-          response_model=dict)
+@router.post("/github", response_model=dict)
 async def process_hook(src: dict):
-    action = src['action']
 
-    # New TR issue opened
-    if action == 'opened':
-        issue = src['issue']
+    event = src['event']
+    payload = src['payload']
 
-        body = issue['body']
-        filename = issue['url']
+    if event == 'issues':
+        action = payload['action']
 
-        with open(filename, 'wb', encoding='utf-8') as f:
-            f.write(body)
+        # New TR issue opened
+        if action == 'opened':
+            issue = src['issue']
 
-            upload_file = UploadFile(f)
+            body = issue['body']
+            filename = issue['url']
 
-            await upload_and_index_document(upload_file, DataType.REQUIREMENTS)
+            with open(filename, 'wb', encoding='utf-8') as f:
+                f.write(body)
 
-    # todo process push webhook
-    # for commit in src['commits']:
-    #   for file in commit['added']:
-    #       add
-    #   for file in commit['removed']:
-    #       remove
-    #   for file in commit['modified']:
-    #       remove, add
+                upload_file = UploadFile(f)
+
+                await upload_and_index_document(upload_file, DataType.REQUIREMENTS)
+
+    elif event == 'push':
+        # fixme
+        for commit in payload['commits']:
+          for file in commit['added']:
+              pass
+              # add
+          for file in commit['removed']:
+              pass
+              # remove
+          for file in commit['modified']:
+              pass
+              # remove, add
